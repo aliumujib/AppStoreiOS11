8ccfed93d6c5ff087bc0c6c57be8137d49c8c79e
Abdul-Mujeeb Aliu <abdulmujibaliu@Abdul-Mujeebs-MacBook-Pro.local>
2017-06-28 21:57:50 +0100

    Initial Commit

---
+++ AppStore iOS 10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStore iOS 10
+//
+//  Created by Abdul-Mujeeb Aliu on 6/28/17.
+//  Copyright © 2017 Abdul-Mujeeb Aliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
